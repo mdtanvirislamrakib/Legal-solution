3dda597cb33f99308544037631353bbffca55fc4
mdtanvirislamrakib <mdtanvirislamrakib7@gmail.com>
2025-01-30 17:29:45 +0600

    footer section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
             clifford: '#da373d',
             'btn-bg': '#B68C5A',
             'gradient-1': "rgba(17, 17, 17, 0.8)",
+            'footer-color': 'rgb(17, 17, 17)',
         },
         fontFamily: {
             'font-Poppins': ["Poppins", "serif"],
